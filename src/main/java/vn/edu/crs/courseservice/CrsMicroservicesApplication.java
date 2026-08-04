72badce4f7b88c0fe085164312c132c6d8cce7d4
PhuongThao30905 <tranphuongthao3092005@gmail.com>
2026-08-04 14:07:19 +0700

    Initial commit

---
+++ src/main/java/vn/edu/crs/courseservice/CrsMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.crs.courseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrsMicroservicesApplication.class, args);
+    }
+
+}
