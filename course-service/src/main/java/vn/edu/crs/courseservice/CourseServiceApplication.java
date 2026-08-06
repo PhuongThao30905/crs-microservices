8bec9691a31faff1ac70d6855bdb5e50e831f47c
PhuongThao30905 <tranphuongthao3092005@gmail.com>
2026-08-06 10:00:47 +0700

    feat(course-service): CRUD course 3-layer + DTO

--- course-service/src/main/java/vn/edu/crs/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/vn/edu/crs/courseservice/CourseServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourseServiceApplication.class, args);
+        SpringApplication.run(
+                CourseServiceApplication.class,
+                args
+        );
     }
-
 }
