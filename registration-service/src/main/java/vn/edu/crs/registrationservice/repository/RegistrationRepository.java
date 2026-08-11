b00efe4b03e8079723ef8edbedeff81e433c01f2
PhuongThao30905 <tranphuongthao3092005@gmail.com>
2026-08-11 10:57:34 +0700

    init(registration-service): registration with cross-service call

--- registration-service/src/main/java/vn/edu/crs/registrationservice/repository/RegistrationRepository.java
+++ registration-service/src/main/java/vn/edu/crs/registrationservice/repository/RegistrationRepository.java
@@ -1,4 +1,18 @@
 package vn.edu.crs.registrationservice.repository;
 
-public class RegistrationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import vn.edu.crs.registrationservice.entity.Registration;
+
+import java.util.List;
+
+public interface RegistrationRepository
+        extends JpaRepository<Registration, Long> {
+
+    List<Registration> findByStudentId(Long studentId);
+
+    boolean existsByStudentIdAndCourseIdAndTrangThai(
+            Long studentId,
+            Long courseId,
+            String trangThai
+    );
 }
